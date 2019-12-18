85d5083e4d34e854e274e29ad7410b2a82b2b1d7
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2019-12-18 10:41:45 +0100

    Store a loaded plugin per source and init it with source config

--- app/metricq/configurator.py
+++ app/metricq/configurator.py
@@ -55,6 +55,7 @@ class Configurator(ManagementAgent):
         super().__init__(
             token,
             management_url,
+            "",
             couchdb_url,
             couchdb_user,
             couchdb_password,
@@ -179,26 +180,21 @@ class Configurator(ManagementAgent):
                 doc[key] = value
 
     async def get_source_plugin(self, source_id) -> SourcePlugin:
-        config = await self.get_configs(source_id)
+        config = (await self.get_configs(source_id))[source_id]
         # TODO fix type extraction
         source_type = "http"
 
-        if source_type not in self._loaded_plugins:
-            full_modul_name = "metricq_wizard_plugin_{}".format(source_type)
-            if importlib.util.find_spec(full_modul_name):
-                plugin_module = importlib.import_module(full_modul_name)
-                self._loaded_plugins[source_type] = plugin_module.get_plugin()
+        if source_id not in self._loaded_plugins:
+            full_module_name = "metricq_wizard_plugin_{}".format(source_type)
+            if importlib.util.find_spec(full_module_name):
+                plugin_module = importlib.import_module(full_module_name)
+                self._loaded_plugins[source_id] = plugin_module.get_plugin(config)
             else:
                 logger.error(
-                    f"Plugin {full_modul_name} for source {source_id} not found."
+                    f"Plugin {full_module_name} for source {source_id} not found."
                 )
 
-        if source_type in self._loaded_plugins:
-            return self._loaded_plugins[source_type]
+        if source_id in self._loaded_plugins:
+            return self._loaded_plugins[source_id]
 
         raise
-
-    async def reload_loaded_plugins(self):
-        for plugin in self._loaded_plugins:
-            logger.debug(f"Reloading {plugin}")
-            importlib.reload(self._loaded_plugins[plugin])
