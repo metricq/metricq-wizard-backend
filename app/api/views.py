8b14b24dcae1e32d59803bdd517c96102fa17b57
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2019-12-19 14:50:37 +0100

    Implements add source config item

--- app/api/views.py
+++ app/api/views.py
@@ -191,7 +191,15 @@ async def get_source_add_config_item_input_form(request: Request):
 @swagger_path("api_doc/add_source_config_item.yaml")
 @routes.post("/api/source/{source_id}/config_items")
 async def add_source_config_item(request: Request):
-    pass
+    source_id = request.match_info["source_id"]
+    configurator: Configurator = request.app["metricq_client"]
+    source_plugin = await configurator.get_source_plugin(source_id=source_id)
+
+    request_data = await request.json()
+
+    config_item = await source_plugin.add_config_item(request_data)
+
+    return Response(text=config_item.json(), content_type="application/json")
 
 
 @routes.get("/api/source/{source_id}/config_item/{config_item_id}/input_form")
