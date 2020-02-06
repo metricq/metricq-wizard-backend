ad7204161f46e9ffc31c9f2699035cdea811627e
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2020-02-06 10:25:52 +0100

    Add more validation for settings

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlparse
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, stricturl, AnyHttpUrl
 
 
 class Settings(BaseSettings):
@@ -12,8 +12,10 @@ class Settings(BaseSettings):
     auth_key = "u0zlRwkhWiMCaIfNuS_eIF9PXMVzw9aSYA-88jI-Y48="
     cookie_name = "metricq_wizard_backend"
     token = name
-    amqp_server = "amqp://guest:guest@localhost/"
-    couchdb_url = "http://localhost:5984"
+    amqp_server: stricturl(
+        tld_required=False, allowed_schemes={"amqp", "amqps"}
+    ) = "amqp://guest:guest@localhost/"
+    couchdb_url: AnyHttpUrl = "http://localhost:5984"
     couchdb_user = "admin"
     couchdb_password = "admin"
     dry_run = False
