f3cd71b5bc1dbe1b4f953158d8d822a2e5be1576
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2020-03-10 16:15:58 +0100

    Adds transformer list endpoint

--- app/api/views/__init__.py
+++ app/api/views/__init__.py
@@ -1,9 +1,11 @@
 from .client import routes as client_routes
 from .metric import routes as metric_routes
 from .source import routes as source_routes
+from .transformer import routes as transformer_routes
 
 
 def add_routes_to_app(app):
     app.router.add_routes(client_routes)
     app.router.add_routes(metric_routes)
     app.router.add_routes(source_routes)
+    app.router.add_routes(transformer_routes)
---
+++ app/api/views/transformer.py
@@ -0,0 +1,49 @@
+# metricq-wizard
+# Copyright (C) 2019 ZIH, Technische Universitaet Dresden, Federal Republic of Germany
+#
+# All rights reserved.
+#
+# This file is part of metricq-wizard.
+#
+# metricq-wizard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# metricq-wizard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with metricq-wizard.  If not, see <http://www.gnu.org/licenses/>.
+import json
+
+from aiohttp.web_request import Request
+from aiohttp.web_response import Response
+from aiohttp.web_routedef import RouteTableDef
+
+import metricq
+from app.metricq import Configurator
+
+logger = metricq.get_logger()
+logger.setLevel("DEBUG")
+
+routes = RouteTableDef()
+
+
+@routes.get("/api/transformers")
+async def get_transformer_list(request: Request):
+    configurator: Configurator = request.app["metricq_client"]
+    config_dict = await configurator.get_configs()
+    transformer_list = []
+    for config_id, config in config_dict.items():
+        if config_id.startswith("transformer-"):
+            try:
+                transformer_list.append({"id": config_id})
+            except KeyError:
+                logger.error(
+                    f"Config of transformer {config_id} is incorrect! Missing key"
+                )
+
+    return Response(text=json.dumps(transformer_list), content_type="application/json")
